5ab7dc4f8c9d3cd924394a6b1c5247bdd9f76884
poorva-1703 <127202348+poorva-1703@users.noreply.github.com>
2024-11-20 20:22:23 +0530

    Add files via upload

---
+++ PreadSTMT_Main.java
@@ -0,0 +1,16 @@
+package com.jbk;
+
+public class PreadSTMT_Main {
+
+	public static void main(String[] args) throws Exception {
+
+		Crub_PreadSTMT cps = new Crub_PreadSTMT();
+		
+//		cps.insertdata();
+//		cps.updatedata();
+//		cps.deletedata();
+		cps.fetchdata();
+		
+	}
+
+}
